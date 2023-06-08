8bb6159b00115fa344460924a440c5c6e5c9d866
arvinxx <arvinx@foxmail.com>
2023-06-08 22:37:05 +0800

    :wrench: chore: add coverage reporter in vitest.config.ts

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'dumi';
 import { homepage } from './package.json';
 
+const isProd = process.env.NODE_ENV === 'production';
+
+// 不是预览模式 同时是生产环境
+const isProdSite = process.env.PREVIEW !== '1' && isProd;
+
 export default defineConfig({
+  // 部署在非根目录时, base 和 publicPath 都需要配置
+  base: isProdSite ? '/kitchen-flow-editor/' : '/',
+  publicPath: isProdSite ? '/kitchen-flow-editor/' : '/',
   themeConfig: {
     name: 'Kitchen Flow Editor',
     socialLinks: {
