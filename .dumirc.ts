ff2d2e446a5ee6ea57f0592fe528b7f4459583ca
arvinxx <arvinx@foxmail.com>
2023-06-12 20:24:48 +0800

    :memo: docs(.dumirc.ts): add favicons and logo to themeConfig

--- .dumirc.ts
+++ .dumirc.ts
@@ -10,8 +10,10 @@ export default defineConfig({
   // 部署在非根目录时, base 和 publicPath 都需要配置
   base: isProdSite ? '/kitchen-flow-editor/' : '/',
   publicPath: isProdSite ? '/kitchen-flow-editor/' : '/',
+  favicons: ['https://gw.alipayobjects.com/zos/bmw-prod/51a51720-8a30-4430-b6c9-be5712364f04.svg'],
   themeConfig: {
     name: 'Kitchen Flow Editor',
+    logo: 'https://gw.alipayobjects.com/zos/bmw-prod/af1ea898-bf02-45d1-9f30-8ca851c70a5b.svg',
     socialLinks: {
       github: homepage,
     },
