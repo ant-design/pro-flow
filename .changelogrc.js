a1dd2e86582ab4a52409c0b0b1bc4d6f7a5bec27
arvinxx <arvinx@foxmail.com>
2023-06-13 11:45:06 +0800

    :wrench: chore: 更新 changelog 配置

--- .changelogrc.js
+++ .changelogrc.js
@@ -2,4 +2,10 @@
 module.exports = {
   displayTypes: ['feat', 'fix', 'styles', 'pref', 'build'],
   titleLanguage: 'zh-CN',
+  showAuthor: true,
+  showAuthorAvatar: true,
+  showSummary: true,
+  reduceHeadingLevel: true,
+  newlineTimestamp: true,
+  addBackToTop: true,
 };
