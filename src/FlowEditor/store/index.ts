b4e0e3e771d543f3fceb5b31a4a9680184a00b00
arvinxx <arvinx@foxmail.com>
2023-08-07 23:36:04 +0800

    :bug: fix: compatible with zustand v4.4 usage

--- src/FlowEditor/store/index.ts
+++ src/FlowEditor/store/index.ts
@@ -1,4 +1,7 @@
-import { create, StoreApi } from 'zustand';
+import { StoreApi } from 'zustand';
+import { shallow } from 'zustand/shallow';
+import { createWithEqualityFn } from 'zustand/traditional';
+
 import { createContext, optionalDevtools } from 'zustand-utils';
 
 import { InternalFlow } from '../types';
@@ -8,8 +11,9 @@ import { createStore, FlowEditorStore } from './actions';
 export const createFlowEditorStore = (showDevtools = false) => {
   const doc = new DocWithHistoryManager<InternalFlow>();
 
-  return create<FlowEditorStore>()(
+  return createWithEqualityFn<FlowEditorStore>()(
     optionalDevtools(showDevtools)(createStore(doc), { name: 'FLOW_EDITOR' }),
+    shallow,
   );
 };
 
