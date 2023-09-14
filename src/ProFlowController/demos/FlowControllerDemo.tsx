d65b1fde20aa29e2a6cf4c26728d0d9c27582b33
jiangchu <jiangchu.wzy@antgroup.com>
2023-09-14 11:37:40 +0800

    :rocket: feat: add ProFlowController component and demos

---
+++ src/ProFlowController/demos/FlowControllerDemo.tsx
@@ -0,0 +1,26 @@
+import { createStyles } from 'antd-style';
+import { memo } from 'react';
+import { Background, BackgroundVariant } from 'reactflow';
+import ProFlowController from '..';
+import ProFlow from '../../ProFlow';
+
+const useStyles = createStyles(({ css }) => ({
+  container: css`
+    width: 100%;
+    height: 600px;
+  `,
+}));
+
+const FlowControllerDemo = memo(() => {
+  const { styles } = useStyles();
+  return (
+    <div className={styles.container}>
+      <ProFlow>
+        <ProFlowController />
+        <Background id="1" gap={10} color="#f1f1f1" variant={BackgroundVariant.Lines} />
+      </ProFlow>
+    </div>
+  );
+});
+
+export default FlowControllerDemo;
