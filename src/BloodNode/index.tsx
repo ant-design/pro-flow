78f14e967db643672519db0d05a6b3d925a280b8
jiangchu <jiangchu.wzy@antgroup.com>
2023-09-18 15:12:36 +0800

    :bug: fix: build error

--- src/BloodNode/index.tsx
+++ src/BloodNode/index.tsx
@@ -1,6 +1,4 @@
 import { NODE_DANGER, NODE_SELECT, NODE_WARNING, NodeSelect } from '@/ProFlow/constants';
-import { ConfigProvider } from 'antd';
-import { CSSProperties, ReactNode, memo, useRef } from 'react';
 import { useStyles } from './styles';
 
 interface BloodNodeProps {
@@ -27,63 +25,28 @@ export function getClsFromSelectType(select: NodeSelect) {
   }
 }
 
-export interface BloodNodePreviewProps extends BloodNodeProps {
-  /**
-   * 是否激活状态
-   */
-  active?: boolean;
-  /**
-   * 自定义样式
-   */
-  style?: CSSProperties;
-  /**
-   * 自定义样式
-   */
-  onClick?: () => void;
-}
+const BloodNode: React.FC<Partial<BloodNodeProps>> = ({
+  title,
+  logo,
+  description,
+  className,
+  selectType = NodeSelect.SELECT,
+}) => {
+  const { styles, cx } = useStyles();
 
-const Preview = memo<Partial<BloodNodePreviewProps>>(
-  ({ title, logo, description, className, selectType = NodeSelect.SELECT }) => {
-    const { styles, cx } = useStyles();
-    const cardRef = useRef<HTMLDivElement>(null);
-
-    return (
-      <ConfigProvider
-        // 不这样做会导致 select 的下拉菜单被遮挡，并且不会正常放大缩小
-        getPopupContainer={() => {
-          return cardRef.current || document.body;
-        }}
-      >
-        {/* <Card ref={cardRef} title={title} className={} style={style}>
-            {children}
-          </Card> */}
-        <div className={cx(styles.nodeWrap, styles[getClsFromSelectType(selectType)], className)}>
-          <img className={'img'} src={logo} alt="" />
-          <div className={'content'}>
-            <div className={'title'}>
-              <span>{title}</span>
-              {/* {mainDanger && <DangerLogo />}
-          {qualityScore && <ApiScore score={parseFloat(qualityScore)} />} */}
-            </div>
-            <div className={'des'}>{description}</div>
-          </div>
+  return (
+    <div className={cx(styles.nodeWrap, styles[getClsFromSelectType(selectType)], className)}>
+      <img className={'img'} src={logo} alt="" />
+      <div className={'content'}>
+        <div className={'title'}>
+          <span>{title}</span>
+          {/* {mainDanger && <DangerLogo />}
+        {qualityScore && <ApiScore score={parseFloat(qualityScore)} />} */}
         </div>
-      </ConfigProvider>
-    );
-  },
-);
-
-const Default = memo<BloodNodeProps>(({ ...props }) => {
-  return <Preview {...props} />;
-}) as (props: BloodNodePreviewProps) => JSX.Element;
-
-interface IBloodNode {
-  (props: BloodNodeProps): ReactNode;
-  Preview: typeof Preview;
-}
-
-const BloodNode = Default as IBloodNode;
-
-BloodNode.Preview = Preview;
+        <div className={'des'}>{description}</div>
+      </div>
+    </div>
+  );
+};
 
 export default BloodNode;
