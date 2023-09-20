0d32bec07497947aa597593d3e0c9801e70bdf04
jiangchu <jiangchu.wzy@antgroup.com>
2023-09-20 16:42:50 +0800

    :memo: feat: 简化圆角线段组件的demo示例

--- src/RadiusEdge/demos/index.tsx
+++ src/RadiusEdge/demos/index.tsx
@@ -27,82 +27,12 @@ const nodes: ProFlowNode[] = [
     },
   },
   {
-    id: 'b1',
+    id: 'a3',
     data: {
       title: 'XXX_API',
       logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*kgyiRKi04eUAAAAAAAAAAAAADvuvAQ/original',
       describe: 'XXX_XXX_XXX_API',
     },
-  },
-  {
-    id: 'a3',
-    data: {
-      title: 'XXXX产品',
-      logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*ezaYT4wYRBwAAAAAAAAAAAAADvuvAQ/original',
-      describe: '2031030213014',
-    },
-  },
-  {
-    id: 'a4',
-    group: true,
-    data: [
-      {
-        id: 'a5',
-        data: {
-          title: 'XXX数据源',
-          describe: 'cksadjfnf',
-          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*jWDsQ5GTmHMAAAAAAAAAAAAADvuvAQ/original',
-        },
-      },
-      {
-        id: 'a6',
-        data: {
-          title: 'XXX_API',
-          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*kgyiRKi04eUAAAAAAAAAAAAADvuvAQ/original',
-          describe: 'XXX_XXX_XXX_API',
-        },
-      },
-      {
-        id: 'a7',
-        data: {
-          title: 'XXXX产品',
-          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*ezaYT4wYRBwAAAAAAAAAAAAADvuvAQ/original',
-          describe: '2031030213014',
-        },
-      },
-      {
-        id: 'a8',
-        data: {
-          title: 'XXX数据源',
-          describe: 'cksadjfnf',
-          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*jWDsQ5GTmHMAAAAAAAAAAAAADvuvAQ/original',
-        },
-      },
-      {
-        id: 'a9',
-        data: {
-          title: 'XXX_API',
-          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*kgyiRKi04eUAAAAAAAAAAAAADvuvAQ/original',
-          describe: 'XXX_XXX_XXX_API',
-        },
-      },
-      {
-        id: 'a10',
-        data: {
-          title: 'XXXX产品',
-          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*ezaYT4wYRBwAAAAAAAAAAAAADvuvAQ/original',
-          describe: '2031030213014',
-        },
-      },
-      {
-        id: 'a11',
-        data: {
-          title: 'XXXX产品',
-          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*ezaYT4wYRBwAAAAAAAAAAAAADvuvAQ/original',
-          describe: '2031030213014',
-        },
-      },
-    ],
   },
 ];
 
@@ -113,19 +43,9 @@ const edges = [
     target: 'a2',
   },
   {
-    id: 'a1-b1',
-    source: 'a1',
-    target: 'b1',
-  },
-  {
     id: 'a2-a3',
-    source: 'a2',
+    source: 'a1',
     target: 'a3',
-  },
-  {
-    id: 'a3-a4',
-    source: 'a3',
-    target: 'a4',
   },
 ];
 
