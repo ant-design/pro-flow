3880e5c3dcbe81637532ff2efd76ca66454cc80e
ModestFun <61576426+ModestFun@users.noreply.github.com>
2023-12-26 09:57:35 +0000

    Deploying to gh-pages from @ ant-design/pro-flow@5b083e338d59d2d828b38c75d273f5421d5ba69f 🚀

---
+++ MiniMap__demos.3f2640a1.async.js
@@ -0,0 +1,4 @@
+"use strict";(self.webpackChunk_ant_design_pro_flow=self.webpackChunk_ant_design_pro_flow||[]).push([[4004],{19499:function(E,l,_){_.r(l);var s=_(18840),a=_.n(s),r=_(63275),d=_(88645),i=_(73979),m=_(50959),e=_(11527),o,A=(0,i.kc)(function(n){var t=n.css;return{container:t(o||(o=a()([`
+    width: 100%;
+    height: 600px;
+  `])))}}),u=(0,m.memo)(function(){var n=A(),t=n.styles;return(0,e.jsx)("div",{className:t.container,children:(0,e.jsx)(r.Z,{nodes:[{id:"a1",label:"default",data:{title:"XXX_API_b3",logo:"https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*kgyiRKi04eUAAAAAAAAAAAAADvuvAQ/original",describe:"XXX_XXX_XXX_API"}}],edges:[],miniMap:!1,children:(0,e.jsx)(d.Z,{position:{x:0,y:0}})})})});l.default=u}}]);
