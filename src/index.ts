adf2f66770c02cd6f93150cc02d08dac41be4968
jiangchu <jiangchu.wzy@antgroup.com>
2024-01-04 10:59:17 +0800

    :bug: fix: refact type export

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,28 @@
-import {
-  BaseEdge,
-  Connection,
-  EdgeChange,
-  EdgeLabelRenderer,
-  EdgeProps,
-  Handle,
-  NodeChange,
-  Position,
-  getBezierPath,
-  getSmoothStepPath,
-} from 'reactflow';
-
-export * from './Background';
+export { BaseEdge, EdgeLabelRenderer, Handle, getBezierPath, getSmoothStepPath } from 'reactflow';
 export { default as Background } from './Background';
 export { default as BasicNode } from './BasicNode';
 export { default as CanvasLoading } from './CanvasLoading';
-export * from './ControlInput';
 export { default as EditNode } from './EditNode';
 export { default as EditableText } from './EditableText';
-export * from './FlowEditor';
-export * from './FlowPanel';
 export { default as FlowPanel } from './FlowPanel';
-export { FlowEditorStoreProviderProps, FlowStoreProvider } from './FlowStoreProvider';
+export { FlowStoreProvider } from './FlowStoreProvider';
 export { default as FlowView } from './FlowView/FlowView';
 export { useEdgesState, useNodesState } from './FlowView/hooks/useFlowState';
+export { default as Inspector } from './Inspector';
+export { default as MiniMap } from './MiniMap';
+export { NodeField, useNodeFieldStyles } from './NodeField';
+export { default as RadiusEdge } from './RadiusEdge';
+
+export * from './Background';
+export * from './ControlInput';
+export * from './FlowEditor';
+export * from './FlowPanel';
 export * from './FlowView/hooks/useFlowView';
 export * from './FlowView/provider/FlowViewProvider';
 export * from './Input';
-export { default as Inspector } from './Inspector';
 export * from './MiniMap';
-export { default as MiniMap } from './MiniMap';
-export { NodeField, useNodeFieldStyles } from './NodeField';
-export type { ExtraAction } from './NodeField';
-export { default as RadiusEdge } from './RadiusEdge';
 export * from './constants';
-export {
-  BaseEdge,
-  Connection,
-  EdgeChange,
-  EdgeLabelRenderer,
-  EdgeProps,
-  Handle,
-  NodeChange,
-  Position,
-  getBezierPath,
-  getSmoothStepPath,
-};
+
+export type { Connection, EdgeChange, EdgeProps, NodeChange, Position } from 'reactflow';
+export type { FlowEditorStoreProviderProps } from './FlowStoreProvider';
+export type { ExtraAction } from './NodeField';
