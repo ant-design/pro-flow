b5ebb485dd204206560a60b0e015a1f756bddf14
jiangchu <jiangchu.wzy@antgroup.com>
2024-01-10 16:06:39 +0800

    :bug: fix: flow editor demo add node

--- docs/guide/demos/flowEditor/proBase.tsx
+++ docs/guide/demos/flowEditor/proBase.tsx
@@ -2,73 +2,19 @@
  * compact: true
  * defaultShowCode: true
  */
-import { FlowEditor, FlowEditorProvider, FlowPanel, useFlowEditor } from '@ant-design/pro-flow';
-import { useEffect, useState } from 'react';
+import { FlowEditor, FlowEditorProvider, FlowPanel } from '@ant-design/pro-flow';
 import { StringRender } from './StringNode';
 import { BtnGroup } from './btnGroup';
 import useStyles from './css/probase.style';
 
 const ProFlowDemo = () => {
-  const editor = useFlowEditor();
   const { styles } = useStyles();
-  const [optHistory, setOptHistory] = useState<string[]>([]);
-
-  useEffect(() => {
-    console.log(editor);
-    editor.addNode({
-      id: 'a1',
-      type: 'StringNode',
-      position: { x: 200, y: 100 },
-      data: {
-        title: 'String Node',
-        handles: {
-          source: 'a1-source',
-          target: 'a1-target',
-        },
-      },
-    });
-  }, [editor]);
 
   return (
     <div className={styles.container}>
-      <FlowEditor
-        nodeTypes={{ StringNode: StringRender }}
-        miniMap={false}
-        devtools={true}
-        beforeConnect={(e) => {
-          console.log('beforeConnect', e);
-          return true;
-        }}
-        onConnect={(e) => {
-          console.log('onConnect', e);
-        }}
-        afterConnect={(e) => {
-          console.log('afterConnect', e);
-          editor.selectElement(e.id);
-        }}
-        beforeEdgesChange={(e) => {
-          console.log('beforeEdgesChange', e);
-          return true;
-        }}
-        onEdgesChange={(e) => {
-          console.log('onEdgesChange', e);
-        }}
-        afterEdgeChange={(e) => {
-          console.log('afterEdgeChange', e);
-        }}
-        beforeNodesChange={(e) => {
-          console.log('beforeNodesChange', e);
-          return true;
-        }}
-        onNodesChange={(e) => {
-          console.log('onNodesChange', e);
-        }}
-        afterNodesChange={(e) => {
-          console.log('afterNodesChange', e);
-        }}
-      >
+      <FlowEditor nodeTypes={{ StringNode: StringRender }} miniMap={false} devtools={true}>
         <FlowPanel position="top-center">
-          <BtnGroup editor={editor} />
+          <BtnGroup />
         </FlowPanel>
       </FlowEditor>
     </div>
