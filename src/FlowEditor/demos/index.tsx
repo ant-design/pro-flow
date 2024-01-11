c2fba623c590ce76c5fc8c88ffb18c7361193c88
jiangchu <jiangchu.wzy@antgroup.com>
2024-01-11 17:33:15 +0800

    :bug: fix: recover demo config

--- src/FlowEditor/demos/index.tsx
+++ src/FlowEditor/demos/index.tsx
@@ -1,8 +1,14 @@
 /**
  * compact: true
  */
-import { FlowEditor, FlowEditorProvider, Handle, Position } from '@ant-design/pro-flow';
-import { FC } from 'react';
+import {
+  FlowEditor,
+  FlowEditorProvider,
+  Handle,
+  Position,
+  useFlowEditor,
+} from '@ant-design/pro-flow';
+import { FC, useEffect } from 'react';
 import useStyles from './index.style';
 
 const StringRender: FC = (node: any) => {
@@ -29,41 +35,41 @@ const StringRender: FC = (node: any) => {
 const nodeTypes = { StringNode: StringRender };
 
 const ProFlowDemo = () => {
-  // const editor = useFlowEditor();
+  const editor = useFlowEditor();
   const { styles } = useStyles();
 
-  // useEffect(() => {
-  //   editor.addNode({
-  //     id: 'a1',
-  //     type: 'StringNode',
-  //     position: { x: 0, y: 100 },
-  //     data: {
-  //       title: 'String Node',
-  //       handles: {},
-  //     },
-  //   });
-  //   editor.addNode({
-  //     id: 'a2',
-  //     type: 'StringNode',
-  //     position: { x: 0, y: 300 },
-  //     data: {
-  //       title: 'String Node',
-  //       handles: {},
-  //     },
-  //   });
-  //   editor.addEdges({
-  //     'a1-a2': {
-  //       id: 'a1-a2',
-  //       source: 'a1',
-  //       target: 'a2',
-  //       data: {
-  //         source: 'a1',
-  //         target: 'a2',
-  //         label: '123',
-  //       },
-  //     },
-  //   });
-  // }, [editor]);
+  useEffect(() => {
+    editor.addNode({
+      id: 'a1',
+      type: 'StringNode',
+      position: { x: 0, y: 100 },
+      data: {
+        title: 'String Node',
+        handles: {},
+      },
+    });
+    editor.addNode({
+      id: 'a2',
+      type: 'StringNode',
+      position: { x: 0, y: 300 },
+      data: {
+        title: 'String Node',
+        handles: {},
+      },
+    });
+    editor.addEdges({
+      'a1-a2': {
+        id: 'a1-a2',
+        source: 'a1',
+        target: 'a2',
+        data: {
+          source: 'a1',
+          target: 'a2',
+          label: '123',
+        },
+      },
+    });
+  }, [editor]);
 
   return (
     <div className={styles.container}>
