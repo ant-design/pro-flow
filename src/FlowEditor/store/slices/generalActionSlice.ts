d60431d84dcfaf90180744e441ccec577c6ebb62
jiangchu <jiangchu.wzy@antgroup.com>
2024-01-16 15:52:53 +0800

    :bug: feat: add deselect befor deleteSelection

--- src/FlowEditor/store/slices/generalActionSlice.ts
+++ src/FlowEditor/store/slices/generalActionSlice.ts
@@ -131,9 +131,9 @@ export const generalActionSlice: StateCreator<
   },
 
   deleteSelection: () => {
-    const { selectedKeys, flattenEdges, flattenNodes, dispatchNodes, dispatchEdges } = get();
-    console.log(get());
-
+    const { selectedKeys, flattenEdges, flattenNodes, dispatchNodes, dispatchEdges, deselectAll } =
+      get();
+    deselectAll();
     selectedKeys.forEach((id) => {
       if (flattenNodes[id]) dispatchNodes({ type: 'deleteNode', id });
       if (flattenEdges[id]) dispatchEdges({ type: 'deleteEdge', id });
