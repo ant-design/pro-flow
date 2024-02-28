effd14ad5cf9a7816d7bc27f18ef21450c0b3c87
jiangchu <jiangchu.wzy@antgroup.com>
2024-02-28 17:23:22 +0800

    :bug: fix: flow provider ci

--- src/FlowStoreProvider/demos/FlowStoreProvider.tsx
+++ src/FlowStoreProvider/demos/FlowStoreProvider.tsx
@@ -1,7 +1,7 @@
 /**
  * description: FlowStoreProvider 包裹后的子级可以获得 editor 实例，使用 editor 提供的各种数据操作方法
  */
-import { DataPreviewer } from '@ant-design/pro-editor';
+// import { DataPreviewer } from '@ant-design/pro-editor';
 import { FlowStoreProvider, IFlowBasicNode, useFlowEditor } from '@ant-design/pro-flow';
 import { Button } from 'antd';
 import { useState } from 'react';
@@ -19,7 +19,7 @@ const App = () => {
         插入 abc key 为随机数
       </Button>
 
-      <DataPreviewer data={[editor.getFlattenNodes()]} />
+      {/* <DataPreviewer data={[editor.getFlattenNodes()]} /> */}
     </div>
   );
 };
