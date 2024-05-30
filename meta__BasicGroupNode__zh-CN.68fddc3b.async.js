547639486f12b4f3533a27284fd5e643d67731fb
ModestFun <61576426+ModestFun@users.noreply.github.com>
2024-05-30 03:51:39 +0000

    Deploying to gh-pages from @ ant-design/pro-flow@ddc8300070c234006d8045494ac42b20f55fe8c1 🚀

---
+++ meta__BasicGroupNode__zh-CN.68fddc3b.async.js
@@ -0,0 +1,198 @@
+"use strict";(self.webpackChunk_ant_design_pro_flow=self.webpackChunk_ant_design_pro_flow||[]).push([[1178],{11998:function(u,a,n){n.r(a),n.d(a,{demos:function(){return p}});var e=n(90228),A=n.n(e),r=n(87999),l=n.n(r),X=n(75271),p={"basicgroupnode-demo-demos":{component:X.memo(X.lazy(function(){return n.e(3184).then(n.bind(n,56441))})),asset:{type:"BLOCK",id:"basicgroupnode-demo-demos",refAtomIds:["BasicGroupNode"],dependencies:{"index.tsx":{type:"FILE",value:n(59871).Z},"@ant-design/pro-flow":{type:"NPM",value:"1.3.9"},"antd-style":{type:"NPM",value:"3.6.2"},react:{type:"NPM",value:"18.3.1"},"./data.ts":{type:"FILE",value:n(69086).Z}},entry:"index.tsx"},context:{"@ant-design/pro-flow":n(66880),"antd-style":n(26164),react:n(75271),"./data.ts":n(94981)},renderOpts:{compile:function(){var f=l()(A()().mark(function t(){var i,c=arguments;return A()().wrap(function(o){for(;;)switch(o.prev=o.next){case 0:return o.next=2,n.e(9425).then(n.bind(n,59425));case 2:return o.abrupt("return",(i=o.sent).default.apply(i,c));case 3:case"end":return o.stop()}},t)}));function s(){return f.apply(this,arguments)}return s}()}}}},94981:function(u,a,n){n.r(a),n.d(a,{edges:function(){return A},nodes:function(){return e}});var e=[{id:"d1",label:"group1",type:"BasicNodeGroup",data:[{id:"a5",data:{title:"XXX\u6570\u636E\u6E90",description:"cksadjfnf",logo:"https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*jWDsQ5GTmHMAAAAAAAAAAAAADvuvAQ/original"}},{id:"a6",data:{title:"XXX_API",logo:"https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*kgyiRKi04eUAAAAAAAAAAAAADvuvAQ/original",description:"XXX_XXX_XXX_API"}},{id:"a7",data:{title:"XXXX\u4EA7\u54C1",logo:"https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*ezaYT4wYRBwAAAAAAAAAAAAADvuvAQ/original",description:"2031030213014"}},{id:"a8",data:{title:"XXX\u6570\u636E\u6E90",description:"cksadjfnf",logo:"https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*jWDsQ5GTmHMAAAAAAAAAAAAADvuvAQ/original"}},{id:"a9",data:{title:"XXX_API",logo:"https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*kgyiRKi04eUAAAAAAAAAAAAADvuvAQ/original",description:"XXX_XXX_XXX_API"}}]},{id:"d2",label:"group2",type:"BasicNodeGroup",data:[{id:"a5",data:{title:"XXX\u6570\u636E\u6E90",description:"cksadjfnf",logo:"https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*jWDsQ5GTmHMAAAAAAAAAAAAADvuvAQ/original"}},{id:"a6",data:{title:"XXX_API",logo:"https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*kgyiRKi04eUAAAAAAAAAAAAADvuvAQ/original",description:"XXX_XXX_XXX_API"}},{id:"a7",data:{title:"XXXX\u4EA7\u54C1",logo:"https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*ezaYT4wYRBwAAAAAAAAAAAAADvuvAQ/original",description:"2031030213014"}},{id:"a8",data:{title:"XXX\u6570\u636E\u6E90",description:"cksadjfnf",logo:"https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*jWDsQ5GTmHMAAAAAAAAAAAAADvuvAQ/original"}},{id:"a9",data:{title:"XXX_API",logo:"https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*kgyiRKi04eUAAAAAAAAAAAAADvuvAQ/original",description:"XXX_XXX_XXX_API"}},{id:"a10",data:{title:"XXXX\u4EA7\u54C1",logo:"https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*ezaYT4wYRBwAAAAAAAAAAAAADvuvAQ/original",description:"2031030213014"}},{id:"a11",data:{title:"XXXX\u4EA7\u54C1",logo:"https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*ezaYT4wYRBwAAAAAAAAAAAAADvuvAQ/original",description:"2031030213014"}}]}],A=[{id:"b12",source:"d1",target:"d2"}]},99578:function(u,a,n){n.d(a,{F:function(){return l}});var e=n(86470),A=n(10394),r=n(75271),l=function(){var p=A.ZP.useBreakpoint();return(0,r.useMemo)(function(){return(0,e.f)(p)},[p])}},26164:function(u,a,n){n.r(a),n.d(a,{StyleProvider:function(){return e.V9},ThemeProvider:function(){return e.f6},createGlobalStyle:function(){return e.vJ},createInstance:function(){return e.Fs},createStyles:function(){return e.kc},createStylish:function(){return e.Yz},css:function(){return e.iv},cx:function(){return e.cx},extractStaticStyle:function(){return e.Y2},injectGlobal:function(){return e.hi},keyframes:function(){return e.F4},setupStyled:function(){return e.Us},styleManager:function(){return e.Rr},useAntdStylish:function(){return A.n},useAntdTheme:function(){return r.A},useAntdToken:function(){return l.S},useResponsive:function(){return X.F},useTheme:function(){return e.Fg},useThemeMode:function(){return p.r}});var e=n(85833),A=n(6493),r=n(66225),l=n(51395),X=n(99578),p=n(74259)},98114:function(u,a,n){n.d(a,{Z:function(){return A}});var e=n(75271);function A(){const[,r]=e.useReducer(l=>l+1,0);return r}},60488:function(u,a,n){n.d(a,{ZP:function(){return p},c4:function(){return r},m9:function(){return f}});var e=n(75271),A=n(79759);const r=["xxl","xl","lg","md","sm","xs"],l=s=>({xs:`(max-width: ${s.screenXSMax}px)`,sm:`(min-width: ${s.screenSM}px)`,md:`(min-width: ${s.screenMD}px)`,lg:`(min-width: ${s.screenLG}px)`,xl:`(min-width: ${s.screenXL}px)`,xxl:`(min-width: ${s.screenXXL}px)`}),X=s=>{const t=s,i=[].concat(r).reverse();return i.forEach((c,v)=>{const o=c.toUpperCase(),m=`screen${o}Min`,d=`screen${o}`;if(!(t[m]<=t[d]))throw new Error(`${m}<=${d} fails : !(${t[m]}<=${t[d]})`);if(v<i.length-1){const g=`screen${o}Max`;if(!(t[d]<=t[g]))throw new Error(`${d}<=${g} fails : !(${t[d]}<=${t[g]})`);const _=`screen${i[v+1].toUpperCase()}Min`;if(!(t[g]<=t[_]))throw new Error(`${g}<=${_} fails : !(${t[g]}<=${t[_]})`)}}),s};function p(){const[,s]=(0,A.ZP)(),t=l(X(s));return e.useMemo(()=>{const i=new Map;let c=-1,v={};return{matchHandlers:{},dispatch(o){return v=o,i.forEach(m=>m(v)),i.size>=1},subscribe(o){return i.size||this.register(),c+=1,i.set(c,o),o(v),c},unsubscribe(o){i.delete(o),i.size||this.unregister()},unregister(){Object.keys(t).forEach(o=>{const m=t[o],d=this.matchHandlers[m];d==null||d.mql.removeListener(d==null?void 0:d.listener)}),i.clear()},register(){Object.keys(t).forEach(o=>{const m=t[o],d=h=>{let{matches:_}=h;this.dispatch(Object.assign(Object.assign({},v),{[o]:_}))},g=window.matchMedia(m);g.addListener(d),this.matchHandlers[m]={mql:g,listener:d},d(g)})},responsiveMap:t}},[s])}const f=(s,t)=>{if(t&&typeof t=="object")for(let i=0;i<r.length;i++){const c=r[i];if(s[c]&&t[c]!==void 0)return t[c]}}},69390:function(u,a,n){var e=n(75271),A=n(2062),r=n(98114),l=n(60488);function X(){let p=arguments.length>0&&arguments[0]!==void 0?arguments[0]:!0;const f=(0,e.useRef)({}),s=(0,r.Z)(),t=(0,l.ZP)();return(0,A.Z)(()=>{const i=t.subscribe(c=>{f.current=c,p&&s()});return()=>t.unsubscribe(i)},[]),f.current}a.Z=X},10394:function(u,a,n){var e=n(69390);function A(){return(0,e.Z)()}a.ZP={useBreakpoint:A}},56469:function(u,a,n){n.r(a),n.d(a,{texts:function(){return e}});const e=[{value:"\u5C5E\u6027\u540D",paraId:0,tocIndex:1},{value:"\u7C7B\u578B",paraId:0,tocIndex:1},{value:"\u63CF\u8FF0",paraId:0,tocIndex:1},{value:"\u9ED8\u8BA4\u503C",paraId:0,tocIndex:1},{value:"\u5FC5\u9009",paraId:0,tocIndex:1},{value:"id",paraId:0,tocIndex:1},{value:"string",paraId:0,tocIndex:1},{value:"\u8282\u70B9\u6807\u9898",paraId:0,tocIndex:1},{value:"-",paraId:0,tocIndex:1},{value:"-",paraId:0,tocIndex:1},{value:"data",paraId:0,tocIndex:1},{value:"BasicNodeData",paraId:0,tocIndex:1},{value:"\u8282\u70B9\u63CF\u8FF0",paraId:0,tocIndex:1},{value:"-",paraId:0,tocIndex:1},{value:"-",paraId:0,tocIndex:1},{value:"\u5C5E\u6027\u540D",paraId:1,tocIndex:2},{value:"\u7C7B\u578B",paraId:1,tocIndex:2},{value:"\u63CF\u8FF0",paraId:1,tocIndex:2},{value:"\u9ED8\u8BA4\u503C",paraId:1,tocIndex:2},{value:"\u5FC5\u9009",paraId:1,tocIndex:2},{value:"title",paraId:1,tocIndex:2},{value:"string",paraId:1,tocIndex:2},{value:"\u8282\u70B9\u6807\u9898",paraId:1,tocIndex:2},{value:"-",paraId:1,tocIndex:2},{value:"-",paraId:1,tocIndex:2},{value:"describe",paraId:1,tocIndex:2},{value:"string",paraId:1,tocIndex:2},{value:"\u8282\u70B9\u63CF\u8FF0",paraId:1,tocIndex:2},{value:"-",paraId:1,tocIndex:2},{value:"-",paraId:1,tocIndex:2},{value:"logo",paraId:1,tocIndex:2},{value:"string",paraId:1,tocIndex:2},{value:"\u8282\u70B9 logo \u7684 url",paraId:1,tocIndex:2},{value:"-",paraId:1,tocIndex:2},{value:"-",paraId:1,tocIndex:2},{value:"titleSlot",paraId:1,tocIndex:2},{value:"{type: 'left' | 'right', value: React.ReactNode}",paraId:1,tocIndex:2},{value:"\u6807\u9898\u53F3\u4FA7\u7684\u63D2\u69FD\uFF0Cleft \u8DDF\u968F\u5728 title \u6587\u5B57\u7ED3\u5C3E\u5904\uFF0Cright \u5728 title \u884C\u5C3E\u90E8",paraId:1,tocIndex:2},{value:"-",paraId:1,tocIndex:2},{value:"-",paraId:1,tocIndex:2}]},69086:function(u,a){a.Z=`import { BasicGroupNodeData, FlowViewNode } from '@ant-design/pro-flow';
+
+export const nodes: FlowViewNode<BasicGroupNodeData[]>[] = [
+  {
+    id: 'd1',
+    label: 'group1',
+    type: 'BasicNodeGroup',
+    data: [
+      {
+        id: 'a5',
+        data: {
+          title: 'XXX\u6570\u636E\u6E90',
+          description: 'cksadjfnf',
+          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*jWDsQ5GTmHMAAAAAAAAAAAAADvuvAQ/original',
+        },
+      },
+      {
+        id: 'a6',
+        data: {
+          title: 'XXX_API',
+          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*kgyiRKi04eUAAAAAAAAAAAAADvuvAQ/original',
+          description: 'XXX_XXX_XXX_API',
+        },
+      },
+      {
+        id: 'a7',
+        data: {
+          title: 'XXXX\u4EA7\u54C1',
+          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*ezaYT4wYRBwAAAAAAAAAAAAADvuvAQ/original',
+          description: '2031030213014',
+        },
+      },
+      {
+        id: 'a8',
+        data: {
+          title: 'XXX\u6570\u636E\u6E90',
+          description: 'cksadjfnf',
+          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*jWDsQ5GTmHMAAAAAAAAAAAAADvuvAQ/original',
+        },
+      },
+      {
+        id: 'a9',
+        data: {
+          title: 'XXX_API',
+          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*kgyiRKi04eUAAAAAAAAAAAAADvuvAQ/original',
+          description: 'XXX_XXX_XXX_API',
+        },
+      },
+    ],
+  },
+  {
+    id: 'd2',
+    label: 'group2',
+    type: 'BasicNodeGroup',
+    data: [
+      {
+        id: 'a5',
+        data: {
+          title: 'XXX\u6570\u636E\u6E90',
+          description: 'cksadjfnf',
+          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*jWDsQ5GTmHMAAAAAAAAAAAAADvuvAQ/original',
+        },
+      },
+      {
+        id: 'a6',
+        data: {
+          title: 'XXX_API',
+          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*kgyiRKi04eUAAAAAAAAAAAAADvuvAQ/original',
+          description: 'XXX_XXX_XXX_API',
+        },
+      },
+      {
+        id: 'a7',
+        data: {
+          title: 'XXXX\u4EA7\u54C1',
+          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*ezaYT4wYRBwAAAAAAAAAAAAADvuvAQ/original',
+          description: '2031030213014',
+        },
+      },
+      {
+        id: 'a8',
+        data: {
+          title: 'XXX\u6570\u636E\u6E90',
+          description: 'cksadjfnf',
+          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*jWDsQ5GTmHMAAAAAAAAAAAAADvuvAQ/original',
+        },
+      },
+      {
+        id: 'a9',
+        data: {
+          title: 'XXX_API',
+          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*kgyiRKi04eUAAAAAAAAAAAAADvuvAQ/original',
+          description: 'XXX_XXX_XXX_API',
+        },
+      },
+      {
+        id: 'a10',
+        data: {
+          title: 'XXXX\u4EA7\u54C1',
+          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*ezaYT4wYRBwAAAAAAAAAAAAADvuvAQ/original',
+          description: '2031030213014',
+        },
+      },
+      {
+        id: 'a11',
+        data: {
+          title: 'XXXX\u4EA7\u54C1',
+          logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*ezaYT4wYRBwAAAAAAAAAAAAADvuvAQ/original',
+          description: '2031030213014',
+        },
+      },
+    ],
+  },
+];
+
+export const edges = [
+  {
+    id: 'b12',
+    source: 'd1',
+    target: 'd2',
+  },
+];
+`},59871:function(u,a){a.Z=`import { FlowView, FlowViewEdge, FlowViewNode, SelectType } from '@ant-design/pro-flow';
+import { createStyles } from 'antd-style';
+import { useState } from 'react';
+import { edges, nodes } from './data';
+
+const useStyles = createStyles(({ css }) => ({
+  container: css\`
+    width: 100%;
+    height: 600px;
+    .ant-progress-text {
+      text-align: center !important;
+    }
+  \`,
+}));
+
+const ProFlowDemo = () => {
+  const [_nodes, setNodes] = useState<FlowViewNode[]>(nodes);
+  const [_edges, setEdges] = useState<FlowViewEdge[]>(edges);
+  const { styles } = useStyles();
+
+  const handleHighlight = (node: FlowViewNode) => {
+    setNodes(
+      _nodes.map((_node) => {
+        if (_node.id === node.id) {
+          _node.select = SelectType.SELECT;
+        } else {
+          _node.select = SelectType.SUB_SELECT;
+        }
+        return _node;
+      }),
+    );
+    setEdges(
+      _edges.map((edge) => {
+        if (edge.source === node.id || edge.target === node.id) {
+          edge.select = SelectType.SUB_SELECT;
+        }
+        return {
+          ...edge,
+        };
+      }),
+    );
+  };
+
+  const handleUnHighlight = () => {
+    setNodes(
+      _nodes.map((_node) => {
+        _node.select = SelectType.DEFAULT;
+        return _node;
+      }),
+    );
+    setEdges(
+      _edges.map((edge) => {
+        edge.select = SelectType.DEFAULT;
+        return edge;
+      }),
+    );
+  };
+
+  return (
+    <div className={styles.container}>
+      <FlowView
+        onNodeDragStart={(e, node: any) => handleHighlight(node)}
+        onPaneClick={handleUnHighlight}
+        nodes={_nodes}
+        edges={_edges}
+      ></FlowView>
+    </div>
+  );
+};
+
+const FlowDemo = () => {
+  return <ProFlowDemo />;
+};
+
+export default FlowDemo;
+`}}]);
---
+++ meta__BasicNode__zh-CN.d9bd5ed8.async.js
@@ -0,0 +1,153 @@
+"use strict";(self.webpackChunk_ant_design_pro_flow=self.webpackChunk_ant_design_pro_flow||[]).push([[4623],{89424:function(s,t,e){e.r(t),e.d(t,{demos:function(){return l}});var n=e(90228),d=e.n(n),o=e(87999),a=e.n(o),r=e(75271),l={"basicnode-demo-default":{component:r.memo(r.lazy(function(){return e.e(9199).then(e.bind(e,90878))})),asset:{type:"BLOCK",id:"basicnode-demo-default",refAtomIds:["BasicNode"],dependencies:{"index.tsx":{type:"FILE",value:e(69275).Z},"@ant-design/pro-flow":{type:"NPM",value:"1.3.9"},"antd-style":{type:"NPM",value:"3.6.2"},react:{type:"NPM",value:"18.3.1"},"./data.tsx":{type:"FILE",value:e(87474).Z},antd:{type:"NPM",value:"5.17.4"}},entry:"index.tsx"},context:{"@ant-design/pro-flow":e(66880),"antd-style":e(26164),react:e(75271),"./data.tsx":e(59941),antd:e(93825)},renderOpts:{compile:function(){var c=a()(d()().mark(function A(){var m,g=arguments;return d()().wrap(function(u){for(;;)switch(u.prev=u.next){case 0:return u.next=2,e.e(9425).then(e.bind(e,59425));case 2:return u.abrupt("return",(m=u.sent).default.apply(m,g));case 3:case"end":return u.stop()}},A)}));function i(){return c.apply(this,arguments)}return i}()}}}},59941:function(s,t,e){e.r(t),e.d(t,{edges:function(){return r},nodes:function(){return a}});var n=e(73954),d=e(52676),o=function(c){var i=c.score;return(0,d.jsx)(n.Z,{style:{fontSize:"6px",textAlign:"center"},type:"circle",trailColor:"white",percent:i===0?1:i*20,strokeColor:i*20>60?"#30a46c":"#e5484d",format:function(){return"".concat(i)},size:28})},a=[{id:"b1",label:"titleSlot-left",data:{title:"XXX_API1",logo:"https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*kgyiRKi04eUAAAAAAAAAAAAADvuvAQ/original",describe:"XXX_XXX_XXX_API",titleSlot:{type:"left",value:(0,d.jsx)("div",{className:"dangerLogo",children:(0,d.jsx)("img",{src:"https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*wkEJRbSowekAAAAAAAAAAAAADvuvAQ/original",alt:""})})}}},{id:"b2",label:"titleSlot-right",data:{title:"XXX_APIddddddddddddddddddddddddddddddddddddddddddddddddddd_b2",logo:"https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*kgyiRKi04eUAAAAAAAAAAAAADvuvAQ/original",describe:"XXX_XXX_XXX_API",titleSlot:{type:"right",value:(0,d.jsx)(o,{score:4})}}},{id:"b3",label:"default",data:{title:"XXX_API_b3",logo:"https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*kgyiRKi04eUAAAAAAAAAAAAADvuvAQ/original",describe:"XXX_XXX_XXX_API"}}],r=[{id:"b12",source:"b1",target:"b2"},{id:"b23",source:"b2",target:"b3"}]},99578:function(s,t,e){e.d(t,{F:function(){return a}});var n=e(86470),d=e(10394),o=e(75271),a=function(){var l=d.ZP.useBreakpoint();return(0,o.useMemo)(function(){return(0,n.f)(l)},[l])}},26164:function(s,t,e){e.r(t),e.d(t,{StyleProvider:function(){return n.V9},ThemeProvider:function(){return n.f6},createGlobalStyle:function(){return n.vJ},createInstance:function(){return n.Fs},createStyles:function(){return n.kc},createStylish:function(){return n.Yz},css:function(){return n.iv},cx:function(){return n.cx},extractStaticStyle:function(){return n.Y2},injectGlobal:function(){return n.hi},keyframes:function(){return n.F4},setupStyled:function(){return n.Us},styleManager:function(){return n.Rr},useAntdStylish:function(){return d.n},useAntdTheme:function(){return o.A},useAntdToken:function(){return a.S},useResponsive:function(){return r.F},useTheme:function(){return n.Fg},useThemeMode:function(){return l.r}});var n=e(85833),d=e(6493),o=e(66225),a=e(51395),r=e(99578),l=e(74259)},28991:function(s,t,e){e.r(t),e.d(t,{texts:function(){return n}});const n=[{value:"\u5C5E\u6027\u540D",paraId:0,tocIndex:1},{value:"\u7C7B\u578B",paraId:0,tocIndex:1},{value:"\u63CF\u8FF0",paraId:0,tocIndex:1},{value:"\u9ED8\u8BA4\u503C",paraId:0,tocIndex:1},{value:"\u5FC5\u9009",paraId:0,tocIndex:1},{value:"title",paraId:0,tocIndex:1},{value:"string",paraId:0,tocIndex:1},{value:"\u8282\u70B9\u6807\u9898",paraId:0,tocIndex:1},{value:"-",paraId:0,tocIndex:1},{value:"-",paraId:0,tocIndex:1},{value:"describe",paraId:0,tocIndex:1},{value:"string",paraId:0,tocIndex:1},{value:"\u8282\u70B9\u63CF\u8FF0",paraId:0,tocIndex:1},{value:"-",paraId:0,tocIndex:1},{value:"-",paraId:0,tocIndex:1},{value:"logo",paraId:0,tocIndex:1},{value:"string",paraId:0,tocIndex:1},{value:"\u8282\u70B9 logo \u7684 url",paraId:0,tocIndex:1},{value:"-",paraId:0,tocIndex:1},{value:"-",paraId:0,tocIndex:1},{value:"titleSlot",paraId:0,tocIndex:1},{value:"{type: 'left' | 'right', value: React.ReactNode}",paraId:0,tocIndex:1},{value:"\u6807\u9898\u53F3\u4FA7\u7684\u63D2\u69FD\uFF0Cleft \u8DDF\u968F\u5728 title \u6587\u5B57\u7ED3\u5C3E\u5904\uFF0Cright \u5728 title \u884C\u5C3E\u90E8",paraId:0,tocIndex:1},{value:"-",paraId:0,tocIndex:1},{value:"-",paraId:0,tocIndex:1}]},87474:function(s,t){t.Z=`import { FlowViewNode } from '@ant-design/pro-flow';
+import { Progress } from 'antd';
+
+const ApiScore: React.FC<{ score: number }> = ({ score }) => {
+  return (
+    <Progress
+      style={{
+        fontSize: '6px',
+        textAlign: 'center',
+      }}
+      type="circle"
+      trailColor={'white'}
+      percent={score === 0 ? 1 : score * 20}
+      strokeColor={score * 20 > 60 ? '#30a46c' : '#e5484d'}
+      format={() => \`\${score}\`}
+      size={28}
+    />
+  );
+};
+
+export const nodes: FlowViewNode[] = [
+  {
+    id: 'b1',
+    label: 'titleSlot-left',
+    data: {
+      title: 'XXX_API1',
+      logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*kgyiRKi04eUAAAAAAAAAAAAADvuvAQ/original',
+      describe: 'XXX_XXX_XXX_API',
+      titleSlot: {
+        type: 'left',
+        value: (
+          <div className="dangerLogo">
+            <img
+              src={
+                'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*wkEJRbSowekAAAAAAAAAAAAADvuvAQ/original'
+              }
+              alt=""
+            />
+          </div>
+        ),
+      },
+    },
+  },
+  {
+    id: 'b2',
+    label: 'titleSlot-right',
+    data: {
+      title: 'XXX_APIddddddddddddddddddddddddddddddddddddddddddddddddddd_b2',
+      logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*kgyiRKi04eUAAAAAAAAAAAAADvuvAQ/original',
+      describe: 'XXX_XXX_XXX_API',
+      titleSlot: {
+        type: 'right',
+        value: <ApiScore score={4} />,
+      },
+    },
+  },
+  {
+    id: 'b3',
+    label: 'default',
+    data: {
+      title: 'XXX_API_b3',
+      logo: 'https://mdn.alipayobjects.com/huamei_ntgeqc/afts/img/A*kgyiRKi04eUAAAAAAAAAAAAADvuvAQ/original',
+      describe: 'XXX_XXX_XXX_API',
+    },
+  },
+];
+
+export const edges = [
+  {
+    id: 'b12',
+    source: 'b1',
+    target: 'b2',
+  },
+  {
+    id: 'b23',
+    source: 'b2',
+    target: 'b3',
+  },
+];
+`},69275:function(s,t){t.Z=`import { FlowView, FlowViewEdge, FlowViewNode, SelectType } from '@ant-design/pro-flow';
+import { createStyles } from 'antd-style';
+import { useState } from 'react';
+import { edges, nodes } from './data';
+
+const useStyles = createStyles(({ css }) => ({
+  container: css\`
+    width: 100%;
+    height: 600px;
+    .ant-progress-text {
+      text-align: center !important;
+    }
+  \`,
+}));
+
+const ProFlowDemo = () => {
+  const [_nodes, setNodes] = useState<FlowViewNode[]>(nodes);
+  const [_edges, setEdges] = useState<FlowViewEdge[]>(edges);
+  const { styles } = useStyles();
+
+  const handleHighlight = (node: FlowViewNode) => {
+    setNodes(
+      _nodes.map((_node) => {
+        if (_node.id === node.id) {
+          _node.select = SelectType.SELECT;
+        }
+        return _node;
+      }),
+    );
+    setEdges(
+      _edges.map((edge) => {
+        if (edge.source === node.id || edge.target === node.id) {
+          edge.select = SelectType.SUB_SELECT;
+        }
+        return {
+          ...edge,
+        };
+      }),
+    );
+  };
+
+  const handleUnHighlight = () => {
+    setNodes(
+      _nodes.map((_node) => {
+        _node.select = SelectType.DEFAULT;
+        return _node;
+      }),
+    );
+    setEdges(
+      _edges.map((edge) => {
+        edge.select = SelectType.DEFAULT;
+        return edge;
+      }),
+    );
+  };
+
+  return (
+    <div className={styles.container}>
+      <FlowView
+        onNodeDragStart={(e, node: any) => handleHighlight(node)}
+        onPaneClick={handleUnHighlight}
+        nodes={_nodes}
+        edges={_edges}
+      ></FlowView>
+    </div>
+  );
+};
+
+const FlowDemo = () => {
+  return <ProFlowDemo />;
+};
+
+export default FlowDemo;
+`}}]);
